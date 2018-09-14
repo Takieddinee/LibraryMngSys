bf06cdfc02b3a5047abb57e7777c55c2b59b353a
Takieddinee <et_ould_said@esi.dz>
2018-09-14 23:04:01 +0100

    Patron Index and Detail views + Controller

--- Library/Controllers/PatronController.cs
+++ Library/Controllers/PatronController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models;
+using Library.Models.ViewModels;
 using Library.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,20 +13,53 @@ namespace Library.Controllers
 {
     public class PatronController : Controller
     {
-        private ICheckout _checkouts;
-        private ILibraryAsset _assets;
 
-        public PatronController(ILibraryAsset libraryAsset, ICheckout checkout)
+        private IPatron _patron;
+
+        public PatronController(IPatron patron)
         {
-            _checkouts = checkout;
-            _assets = libraryAsset;
+
+            _patron = patron;
         }
 
 
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            var patrons = _patron.GetAll();
+            var model = patrons.Select(p => new PatronDetailModel
+            {
+                Id = p.Id,
+                LastName = p.LastName,
+                FirstName = p.FirstName,
+                LibraryCardId = p.LibraryCard.Id,
+
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.HomeLibraryBranch.Name
+            }).ToList();
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var p = _patron.Get(id);
+
+            var model = new PatronDetailModel
+            {
+                Id = p.Id,
+                LastName = p.LastName,
+                FirstName = p.FirstName,
+                LibraryCardId = p.LibraryCard.Id,
+                OverdueFees = p.LibraryCard.Fees,
+                HomeLibraryBranch = p.HomeLibraryBranch.Name,
+                Adress = p.Adress,
+                Telephone =p.TelephoneNumber,
+                AssetsCheckouts = _patron.GetCheckouts(id).ToList()?? new List<Checkout>(),
+                checkoutHistories = _patron.GetCheckoutHistories(id).ToList()?? new List<CheckoutHistory>(),
+                Holds = _patron.GetHolds(id).ToList() ?? new List<Hold>()
+            };
+
+            return View(model);
         }
     }
 }
---
+++ Library/Models/ViewModels/PatronDetailModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.ViewModels
+{
+    public class PatronDetailModel
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public int LibraryCardId { get; set; }
+        public String Adress { get; set; }
+        public DateTime MemberSince { get; set; }
+        public String Telephone { get; set; }
+        public String HomeLibraryBranch { get; set; }
+        public decimal OverdueFees { get; set; }
+        public IEnumerable<Checkout> AssetsCheckouts { get; set; }
+        public IEnumerable<CheckoutHistory> checkoutHistories { get; set; }
+        public IEnumerable<Hold> Holds { get; set; }
+    }
+}
